f0f354b92a4272f1beeb9b4c5fe7e076014b24eb
joaco <lopezjoaco02@gmail.com>
2023-11-18 13:45:23 -0300

    validateJWT en rutas de usuario

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -2,6 +2,7 @@ const { Router } = require("express");
 
 const UserController = require("../controllers/user.controller");
 const userController = require("../controllers/user.controller");
+const validateJwt = require("../middlewares/jwtValidator")
 
 const router = Router();
 
@@ -9,10 +10,10 @@ router.post('/create', UserController.createUser)
 
 router.post('/login', UserController.login)
 
-router.get('/getRecetasGuardadas', userController.getRecetasGuardadas)
+router.get('/getRecetasGuardadas', validateJwt, userController.getRecetasGuardadas)
 
-router.post('/postRecetasGuardadas', userController.postRecetasGuardadas)
+router.post('/postRecetasGuardadas', validateJwt, userController.postRecetasGuardadas)
 
-router.put('/change', userController.changeUser)
+router.put('/change', validateJwt, userController.changeUser)
 
 module.exports = router;

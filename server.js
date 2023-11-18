43c7ad1433acc01bd2e8cba1857892f61a61d35f
joaco <lopezjoaco02@gmail.com>
2023-11-18 13:15:25 -0300

    cors

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 require('dotenv').config();
 const express = require('express');
 const {dbConnection} = require('./src/db/config');
+const cors = require('cors')
 
 
 const app = express();
+app.use(cors())
 dbConnection();
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:5173'); // Permite solicitudes desde este origen
+  res.header('Access-Control-Allow-Origin', '*'); // Permite solicitudes desde este origen
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE'); // Métodos permitidos
   res.header('Access-Control-Allow-Headers', 'Content-Type'); // Cabeceras permitidas
   next();
